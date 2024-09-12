9039956968fa51c62c4e9a5d541b09db97666b4e
Eli Newlin <elinewlin@gmail.com>
2024-09-12 14:16:04 -0500

    finished book one

--- rt_in_weekend/include/camera.h
+++ rt_in_weekend/include/camera.h
@@ -17,6 +17,17 @@ class camera {
         int image_width = 100;      //rendered image width in pixel count
         int samples_per_pixel = 10; //count of random samples for each pixel
         int max_depth = 10;         //maximum number of ray bounces into scene
+        
+
+        double  vfov     = 90;
+        point3  lookfrom = point3(0,0,0);
+        point3  lookat   = point3(0,0,-1);
+        vec3    vup      = vec3(0,1,0);
+
+        double defocus_angle = 0;
+        double focus_dist = 10;
+
+
         /* Public Camera Parameters Here */
         void render(const hittable& world) {
             initialize();
@@ -47,11 +58,14 @@ class camera {
         /* Private Camera Variables Here */
 
         int    image_height;        // Rendered image height
+        double pixel_samples_scale; //Color scale factor for a sum of pixel samples
         point3 center;              // Camera center
         point3 pixel00_loc;         // Location of pixel 0, 0
         vec3   pixel_delta_u;       // Offset to pixel to the right
         vec3   pixel_delta_v;       // Offset to pixel below
-        double pixel_samples_scale; //Color scale factor for a sum of pixel samples
+        vec3   u, v, w;
+        vec3   defocus_disk_u;
+        vec3   defocus_disk_v;
 
         void initialize() {
             image_height = int(image_width / aspect_ratio);
@@ -60,45 +74,64 @@ class camera {
 
             pixel_samples_scale = 1.0 / samples_per_pixel;
 
-            center = point3(0, 0, 0);
+            center = lookfrom;
 
             // Determine viewport dimensions.
-            auto focal_length = 1.0;
-            auto viewport_height = 2.0;
+            auto theta = degrees_to_radians(vfov);
+            auto h = std::tan(theta/2);
+            auto viewport_height = 2 * h * focus_dist;
             auto viewport_width = viewport_height * (double(image_width)/image_height);
 
-            // Calculate the vectors across the horizontal and down the vertical viewport edges.
-            auto viewport_u = vec3(viewport_width, 0, 0);
-            auto viewport_v = vec3(0, -viewport_height, 0);
+
+            //calculate the u,v,w unit basis vectors for the camera coordinate frame
+            w = unit_vector(lookfrom - lookat);
+            u = unit_vector(cross(vup, w));
+            v = cross(w,u);
+
+            //calculate the vectors across the horizontal and down the vertical viewport edges
+            vec3 viewport_u = viewport_width * u;
+            vec3 viewport_v = viewport_height * -v;
 
             // Calculate the horizontal and vertical delta vectors from pixel to pixel.
             pixel_delta_u = viewport_u / image_width;
             pixel_delta_v = viewport_v / image_height;
 
             // Calculate the location of the upper left pixel.
-            auto viewport_upper_left =
-                center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+            auto viewport_upper_left = center - (focus_dist * w) - viewport_u/2 - viewport_v/2;
             pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+
+            // Calculate the camera defocus disk basis vectors.
+            auto defocus_radius = focus_dist * std::tan(degrees_to_radians(defocus_angle / 2));
+            defocus_disk_u = u * defocus_radius;
+            defocus_disk_v = v * defocus_radius; 
         }
 
         ray get_ray(int i, int j) const {
-            // Construct a camera ray originating from the origin and directed at randomly sampled
-            // point around the pixel location i, j.
+        // Construct a camera ray originating from the defocus disk and directed at a randomly
+        // sampled point around the pixel location i, j.
+        auto offset = sample_square();
+        auto pixel_sample = pixel00_loc
+                          + ((i + offset.x()) * pixel_delta_u)
+                          + ((j + offset.y()) * pixel_delta_v);
 
-            auto offset = sample_square();
-            auto pixel_sample = pixel00_loc
-                              + ((i + offset.x()) * pixel_delta_u)
-                              + ((j + offset.y()) * pixel_delta_v);
+        auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
+        auto ray_direction = pixel_sample - ray_origin;
 
-            auto ray_origin = center;
-            auto ray_direction = pixel_sample - ray_origin;
-
-            return ray(ray_origin, ray_direction);
+        return ray(ray_origin, ray_direction);
         }
 
         vec3 sample_square() const {
             // Returns the vector to a random point in the [-.5,-.5]-[+.5,+.5] unit square.
             return vec3(random_double() - 0.5, random_double() - 0.5, 0);
+        }
+
+
+        
+        point3 defocus_disk_sample() const {
+            // Returns a random point in the camera defocus disk.
+            auto p = random_in_unit_disk();
+            return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
         }
 
 
--- rt_in_weekend/include/vec3.h
+++ rt_in_weekend/include/vec3.h
@@ -125,6 +125,14 @@ inline vec3 random_unit_vector() {
     }
 }
 
+inline vec3 random_in_unit_disk() {
+    while (true) {
+        auto p = vec3(random_double(-1,1), random_double(-1,1),0);
+        if (p.length_squared() < 1)
+            return p;
+    }
+}
+
 inline vec3 random_on_hemisphere(const vec3& normal) {
     vec3 on_unit_sphere = random_unit_vector();
     if(dot(on_unit_sphere, normal) > 0.0)
@@ -135,6 +143,14 @@ inline vec3 random_on_hemisphere(const vec3& normal) {
 
 inline vec3 reflect(const vec3& v, const vec3& n) {
     return v - 2*dot(v,n)*n;
+}
+
+
+inline vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
+    auto cos_theta = std::fmin(dot(-uv, n), 1.0);
+    vec3 r_out_perp = etai_over_etat * (uv + cos_theta*n);
+    vec3 r_out_parallel = -std::sqrt(std::fabs(1.0 - r_out_perp.length_squared())) * n;
+    return r_out_perp + r_out_parallel;
 }
 
 #endif
